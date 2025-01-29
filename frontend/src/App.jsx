240fa851c0f86857256d97007e70b693192bdcae
SohailDAD <dsohail866@gmail.com>
2025-01-30 00:23:44 +0500

    add or create the Login page or Modal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Home from "./home/Home";
 import { Route, Routes } from 'react-router-dom'
 import Courses from "./courses/Courses";
+import SignUp from "./components/SignUp";
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/courses" element={<Courses />} />
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
 
     </>
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Login() {
     return (
@@ -10,9 +11,28 @@ function Login() {
                         <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                     </form>
                     <h3 className="font-bold text-lg">Login</h3>
-                    <div>
+                    {/* Email */}
+                    <div className='mt-4 space-y-2'>
                         <span>Email</span>
-                        <input type='email' placeholder='Enter your email'/>
+                        <br/>
+                        <input  type='email' placeholder='Enter your email'
+                        className='w-80 px-3 py-1 border rounded-full outline-none'
+                        />
+                    </div>
+                    {/* Password */}
+                    <div className='mt-4 space-y-2'>
+                        <span>Password</span>
+                        <br/>
+                        <input  type='password' placeholder='Enter your password'
+                        className='w-80 px-3 py-1 border rounded-full outline-none'
+                        />
+                    </div>
+                    {/* Button */}
+                    <div className='flex justify-around mt-4 items-center'>
+                        <button className='bg-pink-500 rounded-md p-2 text-white hover:bg-pink-600 duration-300'>Login</button>
+                        <p>
+                            Not registered? <span><Link to='/signup' className='underline text-blue-700'>SignUp</Link></span>
+                        </p>
                     </div>
                 </div>
             </dialog>
