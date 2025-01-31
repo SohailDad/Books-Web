e790d3b083fe16cd25f07f9c999021d256774627
SohailDAD <dsohail866@gmail.com>
2025-01-31 23:40:45 +0500

    error handle in login page

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,39 +1,54 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useForm } from "react-hook-form";
+
 
 function Login() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm();
+    const onSubmit = data => console.log(data);
+
     return (
         <div>
             <dialog id="my_modal_3" className="modal">
                 <div className="modal-box">
-                    <form method="dialog">
+                    <form method="dialog" onSubmit={handleSubmit(onSubmit)}>
                         {/* if there is a button in form, it will close the modal */}
                         <Link to="/" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</Link>
+                        <h3 className="font-bold text-lg">Login</h3>
+                        {/* Email */}
+                        <div className='mt-4 space-y-2'>
+                            <span>Email</span>
+                            <br />
+                            <input type='email' placeholder='Enter your email'
+                                className='w-80 px-3 py-1 border rounded-full outline-none'
+                                {...register("name", { required: true })}
+                            />
+                            <br />
+                            {errors.name && <span className="text-sm text-red-500">This field is required</span>}
+                        </div>
+                        {/* Password */}
+                        <div className='mt-4 space-y-2'>
+                            <span>Password</span>
+                            <br />
+                            <input type='password' placeholder='Enter your password'
+                                className='w-80 px-3 py-1 border rounded-full outline-none'
+                                {...register("name", { required: true })}
+                            />
+                            <br />
+                            {errors.name && <span className="text-sm text-red-500">This field is required</span>}
+                        </div>
+                        {/* Button */}
+                        <div className='flex justify-around mt-4 items-center'>
+                            <button className='bg-pink-500 rounded-md p-2 text-white hover:bg-pink-600 duration-300'>Login</button>
+                            <p>
+                                Not registered?<Link to='/signup' className='underline text-blue-700'>SignUp</Link>
+                            </p>
+                        </div>
                     </form>
-                    <h3 className="font-bold text-lg">Login</h3>
-                    {/* Email */}
-                    <div className='mt-4 space-y-2'>
-                        <span>Email</span>
-                        <br/>
-                        <input  type='email' placeholder='Enter your email'
-                        className='w-80 px-3 py-1 border rounded-full outline-none'
-                        />
-                    </div>
-                    {/* Password */}
-                    <div className='mt-4 space-y-2'>
-                        <span>Password</span>
-                        <br/>
-                        <input  type='password' placeholder='Enter your password'
-                        className='w-80 px-3 py-1 border rounded-full outline-none'
-                        />
-                    </div>
-                    {/* Button */}
-                    <div className='flex justify-around mt-4 items-center'>
-                        <button className='bg-pink-500 rounded-md p-2 text-white hover:bg-pink-600 duration-300'>Login</button>
-                        <p>
-                            Not registered?<Link to='/signup' className='underline text-blue-700'>SignUp</Link>
-                        </p>
-                    </div>
                 </div>
             </dialog>
         </div>
