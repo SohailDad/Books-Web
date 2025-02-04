35b2a16811bae633fc6dd4caa93fb82be113f2ed
SohailDAD <dsohail866@gmail.com>
2025-02-04 22:55:05 +0500

    fixed the file path issue

--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getBook } from "./controller/bookController";
+import { getBook } from "../controller/bookController.js";
 
 
 const router = express.Router();
