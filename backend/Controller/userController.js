ccb4f53150fc51d9aeaf494a8b898d1d6e2cd154
SohailDAD <dsohail866@gmail.com>
2025-02-06 22:57:47 +0500

    make the login function

--- backend/Controller/userController.js
+++ backend/Controller/userController.js
@@ -26,3 +26,24 @@ export const signUp = async (req,res)=>{
         res.status(500).json({message:'Intervals server error...'})
     }
 }
+
+export const login = async(req,res)=>{
+    try {
+        const {email,password} = req.body;
+        const user = await User.findOne({email});
+        const isMatch = await bcryptjs.compare(password,user.password);
+
+        if (!user || !isMatch) {
+            res.status(400).json({message:"Invalid password & username"})
+        }else{
+            res.status(200).json({message:"Login successfull",user:{
+                _id:user._id,
+                fullname:user.fullname,
+                email:user.email
+            }})
+        }
+    } catch (error) {
+        console.log("Error: ",error.message)
+        res.status(500).json({message:"Interval server error..."})
+    }
+}
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { signUp } from "../controller/userController.js";
+import { login, signUp } from "../controller/userController.js";
 
 const router = express.Router()
 
 
 router.post('/signup',signUp);
+router.post('/login',login);
 
 
 export default router;
